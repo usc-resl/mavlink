24b43fad9f5206ef0f8ce94170167b00659dab58
James Goppert <jgoppert@w8.hsl.dynalias.com>
2012-05-18 19:34:28 -0400

    Added branch for boost asio server/ client.
    This should simplify talking to mavlink for C++ programs
    by providing a simple x-platform library. TCP/UDP/Serial support
    will be provided. This can also be used to run test programs.
    It is similar to pymavlink for C++ on a pc.

---
+++ asio/async_echo_server.cpp
@@ -0,0 +1,194 @@
+//
+// async_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include "v1.0/common/mavlink.h"
+
+using boost::asio::ip::udp;
+using boost::asio::ip::tcp;
+
+class ServerUDP
+{
+public:
+  ServerUDP(boost::asio::io_service& io_service, short port)
+    : io_service_(io_service),
+      socket_(io_service, udp::endpoint(udp::v4(), port))
+  {
+    socket_.async_receive_from(
+        boost::asio::buffer(data_, max_length), sender_endpoint_,
+        boost::bind(&ServerUDP::handle_receive_from, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+  void handle_receive_from(const boost::system::error_code& error,
+      size_t bytes_recvd)
+  {
+    if (!error && bytes_recvd > 0)
+    {
+      socket_.async_send_to(
+          boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
+          boost::bind(&ServerUDP::handle_send_to, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      socket_.async_receive_from(
+          boost::asio::buffer(data_, max_length), sender_endpoint_,
+          boost::bind(&ServerUDP::handle_receive_from, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+  }
+
+  void handle_send_to(const boost::system::error_code& /*error*/,
+      size_t /*bytes_sent*/)
+  {
+    socket_.async_receive_from(
+        boost::asio::buffer(data_, max_length), sender_endpoint_,
+        boost::bind(&ServerUDP::handle_receive_from, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  boost::asio::io_service& io_service_;
+  udp::socket socket_;
+  udp::endpoint sender_endpoint_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class Session
+{
+public:
+  Session(boost::asio::io_service& io_service)
+    : socket_(io_service)
+  {
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  void start()
+  {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&Session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error,
+      size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&Session::handle_write, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      socket_.async_read_some(boost::asio::buffer(data_, max_length),
+          boost::bind(&Session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class ServerTCP
+{
+public:
+  ServerTCP(boost::asio::io_service& io_service, short port)
+    : io_service_(io_service),
+      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    Session* new_session = new Session(io_service_);
+    acceptor_.async_accept(new_session->socket(),
+        boost::bind(&ServerTCP::handle_accept, this, new_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(Session* new_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_session->start();
+    }
+    else
+    {
+      delete new_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_udp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    using namespace std; // For atoi.
+    ServerUDP serverUDP(io_service, atoi(argv[1]));
+    ServerTCP serverTCP(io_service, atoi(argv[1]));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
